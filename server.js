14b2a91d6d19d3de5db4af4fd5b6a7146e03caaf
Michael Nguyen <Michael.Nguyen@feisystems.com>
2018-06-05 16:20:31 -0400

    Update demo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var processFolder = require('./libs/process-folder');
 
-// Run with 'npm start demo /Users/michael.nguyen/Desktop/2018-05-Logs/'
+// Run with 'npm start demo ./modules/demo/demo-logs/' or 'npm start ltss-evv ./modules/demo/demo-logs/'
 var moduleName = process.argv[2];
 var folderPath = process.argv[3];
 processFolder(folderPath, require(`./modules/${moduleName}`));
